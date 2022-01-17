c868275fe00682cf5ba56cacea395d1c63f21aae
mpf <michael@friedlander.io>
2022-01-17 01:52:25 +0000

    Deploying to gh-pages from  @ a6451b2feb4c13a3c70ea46c77c32afa7b87266c 🚀

---
+++ assets/notes/regularized-least-squares/code/ls-reg-noisy3.jl
@@ -0,0 +1,10 @@
+# This file was generated, do not modify it. # hide
+λ = 100
+plot(t, b, leg =:topleft, label="noisy data")
+b̂ = [ b; zeros(n)]
+for λ in LinRange(100,5000,5) 
+    Â = [ I; √λ*D ]
+    xLS = Â \ b̂
+    plot!(t, xLS, label="λ = $(λ)")
+end
+savefig(joinpath(@OUTPUT,"ls-reg-noisy3")) # hide
